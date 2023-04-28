2603c58215561bd65e014621384ab1324fb75593
Mohd Owais <78542017+MohdOwais22@users.noreply.github.com>
2023-04-28 22:58:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def submit():
             'supplement_buy_link': supplement_buy_link
         })
     if __name__ == '__main__':
-        app.run(debug=True, host="0.0.0.0", port=8000)
+        app.run(debug=True, port=8000)
