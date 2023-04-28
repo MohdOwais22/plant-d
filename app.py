9c074e0d80add729f99a92fd3da16a80e703145a
Mohd Owais <78542017+MohdOwais22@users.noreply.github.com>
2023-04-28 22:53:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, jsonify
 from PIL import Image
 import torchvision.transforms.functional as TF
 import CNN
@@ -29,19 +29,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template('home.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact-us.html')
-
-@app.route('/index')
-def ai_engine_page():
-    return render_template('index.html')
-
-@app.route('/mobile-device')
-def mobile_device_detected_page():
-    return render_template('mobile-device.html')
+    return "working"
 
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
@@ -59,13 +47,14 @@ def submit():
         supplement_name = supplement_info['supplement name'][pred]
         supplement_image_url = supplement_info['supplement image'][pred]
         supplement_buy_link = supplement_info['buy link'][pred]
-        return render_template('submit.html' , title = title , desc = description , prevent = prevent , 
-                               image_url = image_url , pred = pred ,sname = supplement_name , simage = supplement_image_url , buy_link = supplement_buy_link)
-
-@app.route('/market', methods=['GET', 'POST'])
-def market():
-    return render_template('market.html', supplement_image = list(supplement_info['supplement image']),
-                           supplement_name = list(supplement_info['supplement name']), disease = list(disease_info['disease_name']), buy = list(supplement_info['buy link']))
-
-if __name__ == '__main__':
+        return jsonify({
+            'title': title,
+            'description': description,
+            'prevent': prevent,
+            'pred': int(pred),
+            'supplement_name': supplement_name,
+            'supplement_image_url': supplement_image_url,
+            'supplement_buy_link': supplement_buy_link
+        })
+    if __name__ == '__main__':
     app.run(debug=True)
