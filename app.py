efcebb939bf60ac410b6141e47ac7fd12cde18eb
Mohd Owais <78542017+MohdOwais22@users.noreply.github.com>
2023-04-28 22:55:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ disease_info = pd.read_csv('disease_info.csv' , encoding='cp1252')
 supplement_info = pd.read_csv('supplement_info.csv',encoding='cp1252')
 
 model = CNN.CNN(39)    
-model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
+model.load_state_dict(torch.load("plant_disease_model_1_latest.pt", map_location=torch.device("cpu")))
 
 def prediction(image_path):
     image = Image.open(image_path)
